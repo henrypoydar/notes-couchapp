b8e148624ffffb913f0ebd296fa7d9f3efb23a29
hpoydar <hpoydar@gmail.com>
2009-04-30 22:28:44 -0400

    Learning some more ...

---
+++ shows/new.js
@@ -0,0 +1,18 @@
+function(doc, req) {  
+  // !json templates.post
+  // !json blog
+  // !code vendor/couchapp/template.js
+  // !code vendor/couchapp/path.js
+
+  // we only show html
+  return template(templates.new, {
+    title : doc.title,
+    blogName : blog.title,
+    post : doc.html,
+    date : doc.created_at,
+    author : doc.author,
+    assets : assetPath(),
+    editPostPath : showPath('edit', doc._id),
+    index : listPath('index','recent-posts',{descending:true, limit:5})
+  });
+}
